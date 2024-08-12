c2b7ab50bfc5f3280975b81fe235dfc6905e236a
Matias <matute_11@hotmail.com.ar>
2024-08-12 18:09:31 -0300

    Adding routes and the new logo

--- anacoffee-react/src/layouts/layout.jsx
+++ anacoffee-react/src/layouts/layout.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom'
 
-export default function layout() {
+export default function Layout() {
   return (
     <div>
-      
+      Layout
+
+      <Outlet />
     </div>
   )
 }
--- anacoffee-react/tailwind.config.js
+++ anacoffee-react/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts}",
+    "./src/**/*.{jsx,tsx}",
   ],
   theme: {
     extend: {},
