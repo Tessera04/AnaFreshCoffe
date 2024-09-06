7af0973e85a51c9482480bb082ebd4797a60c96c
Matias <matute_11@hotmail.com.ar>
2024-09-06 16:38:33 -0300

    Finishing connections on products page

--- anacoffee-react/src/views/Inicio.jsx
+++ anacoffee-react/src/views/Inicio.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { productos as data } from '../data/Productos'
+import useSWR from 'swr';
 import Producto from '../components/Producto'
 import useKiosko from '../hooks/useKiosko';
+import clienteAxios from '../config/axios';
 
 export default function Inicio() {
 
   const { categoriaActual } = useKiosko();
 
-  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+  //Consulta SWR
+  const fetcher = () => clienteAxios('/api/productos').then(data => data.data);
+  const { data, error, isLoading } = useSWR('/api/productos', fetcher, {
+    refreshInterval: 1000, // refresca cada 1 segundo
+  });
+
+  if(isLoading) return 'Cargando...'
+
+  const productos = data.data.filter(producto => producto.categoria_id === categoriaActual.id)
 
   return (
     <>
